943eb4ef364bfd412f368ea76cb7d68da879aaf5
Paul Bütof <paul.buetof@righthandrobotics.com>
2022-04-01 18:36:38 +0200

    Add email notifications

--- app.py
+++ app.py
@@ -4,17 +4,25 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from base64 import b64decode
 from twocaptcha import TwoCaptcha
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
 from os import environ
 from time import sleep
 import re
 
 
 TWOCAPTCHA_API_KEY = environ["TWOCAPTCHA_API_KEY"]
+SENDGRID_API_KEY = environ["SENDGRID_API_KEY"]
+NOTIFICATION_RECIPIENTS = environ["NOTIFICATION_RECIPIENTS"].split(":")
 
 
-driver = webdriver.Firefox()
+options = webdriver.FirefoxOptions()
+options.add_argument('-headless')
+driver = webdriver.Firefox(options=options)
+
 
 last_checked_month = None
+results = []
 
 for year in range(2022, 2024):
     for month in range(1, 13):
@@ -22,7 +30,7 @@ for year in range(2022, 2024):
         # load captcha page
         url = f"https://service2.diplo.de/rktermin/extern/appointment_showMonth.do?locationCode=amst&realmId=1113&categoryId=2662&dateStr=01.{str(month).zfill(2)}.{year}"
         driver.get(url)
-        sleep(0.5)
+        sleep(1)
 
         # check for captcha
         captchas = driver.find_elements(By.CSS_SELECTOR, "captcha div")
@@ -56,7 +64,7 @@ for year in range(2022, 2024):
                     if "wrong" in error_messages[0].text:
                         captcha_solved = False
 
-                print("done" if captcha_solved else "error")
+                print("done" if captcha_solved else "failed")
 
                 # report captcha success
                 solver.report(captcha_solution["captchaId"], captcha_solved)
@@ -72,8 +80,19 @@ for year in range(2022, 2024):
 
         if not any(["Unfortunately, there are no appointments" in h.text for h in headers]):
             print(f"✅ Appointments available in {current_checked_month} - {url}")
+            results.append((current_checked_month, url))
         else:
             print(f"❎ No Appointments in {current_checked_month}")
+
+if len(results):
+    email = Mail(
+        from_email="irrer.polterer@posteo.de",
+        to_emails=NOTIFICATION_RECIPIENTS,
+        subject="[NOTIFICATION] Konsulat Termine",
+        html_content="<br><br>".join([f"<a href='{url}'>Appointments available in {month}</a>" for (month, url) in results])
+    )
+    sg = SendGridAPIClient(SENDGRID_API_KEY)
+    sg.send(email)
 
 # end
 driver.quit()
