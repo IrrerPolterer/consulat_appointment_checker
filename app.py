c0e94c017c50cce6ff86868acccc47aa0f94e47e
Paul Bütof <paul.buetof@righthandrobotics.com>
2022-04-01 12:51:57 +0200

    Check for Appointments within 2-year span

--- app.py
+++ app.py
@@ -1,23 +1,79 @@
+from ftplib import error_reply
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 from base64 import b64decode
-from captcha_solver import CaptchaSolver
+from twocaptcha import TwoCaptcha
+from os import environ
+from time import sleep
 import re
+
+
+TWOCAPTCHA_API_KEY = environ["TWOCAPTCHA_API_KEY"]
 
 
 driver = webdriver.Firefox()
 
-### Captcha Page
+last_checked_month = None
 
-driver.get("https://service2.diplo.de/rktermin/extern/appointment_showMonth.do?locationCode=amst&realmId=1113&categoryId=2662")
+for year in range(2022, 2023):
+    for month in range(1, 12):
 
-# save captcha
-captcha = driver.find_element(By.CSS_SELECTOR, "captcha div")
-captcha_image_data = re.search("url\(\"data:image/jpg;base64,(.*)\"\)", captcha.value_of_css_property("background")).group(1)
-with open("captcha.jpg", "wb") as img:
-    img.write(b64decode(captcha_image_data))
+        # load captcha page
+        url = f"https://service2.diplo.de/rktermin/extern/appointment_showMonth.do?locationCode=amst&realmId=1113&categoryId=2662&dateStr=01.{str(month).zfill(2)}.{year}"
+        driver.get(url)
+        sleep(2)
 
+        # check for captcha
+        captchas = driver.find_elements(By.CSS_SELECTOR, "captcha div")
+        if len(captchas):
 
+            captcha_solved = False
+            while captcha_solved == False:
+                print("🔃 Solving Captcha... ", end="")
+
+                # save captcha
+                captcha = captchas[0]
+                captcha_image_data = re.search("url\(\"data:image/jpg;base64,(.*)\"\)", captcha.value_of_css_property("background")).group(1)
+                with open("captcha.jpg", "wb") as img:
+                    img.write(b64decode(captcha_image_data))
+
+                # solve captcha
+                solver = TwoCaptcha(TWOCAPTCHA_API_KEY)
+                captcha_solution = solver.normal("captcha.jpg")
+
+                # enter captcha
+                field = driver.find_element(By.ID, "appointment_captcha_month_captchaText")
+                field.clear()
+                field.send_keys(captcha_solution["code"])
+                field.send_keys(Keys.RETURN)
+                sleep(5)
+
+                # check captcha
+                captcha_solved = True
+                error_messages = driver.find_elements(By.ID, "message")
+                if len(error_messages):
+                    if "wrong" in error_messages[0].text:
+                        captcha_solved = False
+
+                print("done" if captcha_solved else "error")
+
+                # report captcha success
+                solver.report(captcha_solution["captchaId"], captcha_solved)
+
+        # check for appointments
+        headers = driver.find_elements(By.CSS_SELECTOR, "h2")
+
+        # skip if already checked
+        current_checked_month = headers[-1].text
+        if current_checked_month == last_checked_month:
+            continue
+        last_checked_month = current_checked_month
+
+        if not any(["Unfortunately, there are no appointments" in h.text for h in headers]):
+            print(f"✅ Appointments available in {current_checked_month} - {url}")
+        else:
+            print(f"❎ No Appointments in {current_checked_month}")
 
 # end
 driver.quit()
